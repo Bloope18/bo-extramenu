8e7f82fc1d08bffe79d6ad1f9003319f9e861828
talevi <122776166+talevi18@users.noreply.github.com>
2025-07-22 23:50:34 +0300

    Update config.lua
    Add keybind options to config.lua.

--- config.lua
+++ config.lua
@@ -1,4 +1,7 @@
 return {
     command = 'extramenu', -- Command to open the extramenu.
-    commandEnabled = true -- Set to true if the command should be enabled, false to disable it.
+    commandEnabled = true, -- Set to true if the command should be enabled, false to disable it.
+
+    keybindEnabled = true, -- Set to true if you want to use a keybind to open the extramenu.
+    keybindKey = 'F2' -- The key to use for the keybind
 }
